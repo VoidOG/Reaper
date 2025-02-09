39aaa36684744c19a7f34d0ebd3233d70ef56ff8
VoidOG <VoidGod123@outlook.com>
2025-02-09 12:38:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\ʀᴇᴀᴘᴇʀ ꜱᴘᴀᴍ ʙᴏᴛ ᴅᴇᴘʟᴏʏᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ⚡")
+print("ʀᴇᴀᴘᴇʀ ꜱᴘᴀᴍ ʙᴏᴛ ᴅᴇᴘʟᴏʏᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ⚡")
 
 
 async def main():
